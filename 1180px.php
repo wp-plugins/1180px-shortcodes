7dbe13884bbad5d09ad6b9c98dd317b6024de4b3
chrisblackwell <chrisblackwell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-06 13:37:35 +0000

    Adding version 1.0 of 1180px shortcodes
    git-svn-id: https://plugins.svn.wordpress.org/1180px-shortcodes/trunk@692711 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1180px.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: 1180px Shortcodes
+Plugin URI: http://1180px.com
+Description: Adds simple shortcodes for the 1180px css framework
+Version: 0.8
+Author: Chris Blackwell
+Author URI: http://chrisblackwell.me
+*/
+
+/**
+ * Copyright (c) 2013 Chris Blackwell. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
+// Register the 1180px css file
+wp_register_style( '1180px', plugins_url( '/css/1180px.min.css', __FILE__ ), array(), '1.0', 'all' );
+wp_enqueue_style( '1180px' );
+
+// [row]
+function row_shortcode( $atts ) {
+    return '<div class="row">';
+}
+add_shortcode( 'row', 'row_shortcode' );
+
+// [/row]
+function row_close_shortcode( $atts ) {
+    return '</div><!-- close row -->';
+}
+add_shortcode( '/row', 'row_close_shortcode' );
+
+// [/span]
+function span_close_shortcode( $atts ) {
+    return '</div><!-- close span -->';
+}
+add_shortcode( '/row', 'span_close_shortcode' );
+
+// [span1]
+function span1_shortcode( $atts ) {
+    return '<div class="span1">';
+}
+add_shortcode( 'span1', 'span1_shortcode' );
+
+// [span2]
+function span2_shortcode( $atts ) {
+    return '<div class="span2">';
+}
+add_shortcode( 'span2', 'span2_shortcode' );
+
+// [span3]
+function span3_shortcode( $atts ) {
+    return '<div class="span3">';
+}
+add_shortcode( 'span3', 'span3_shortcode' );
+
+// [span1]
+function span4_shortcode( $atts ) {
+    return '<div class="span4">';
+}
+add_shortcode( 'span4', 'span4_shortcode' );
+
+// [span5]
+function span5_shortcode( $atts ) {
+    return '<div class="span5">';
+}
+add_shortcode( 'span5', 'span5_shortcode' );
+
+// [span6]
+function span6_shortcode( $atts ) {
+    return '<div class="span6">';
+}
+add_shortcode( 'span6', 'span6_shortcode' );
+
+// [span7]
+function span7_shortcode( $atts ) {
+    return '<div class="span7">';
+}
+add_shortcode( 'span7', 'span7_shortcode' );
+
+// [span8]
+function span8_shortcode( $atts ) {
+    return '<div class="span8">';
+}
+add_shortcode( 'span8', 'span8_shortcode' );
+
+// [span9]
+function span9_shortcode( $atts ) {
+    return '<div class="span9">';
+}
+add_shortcode( 'span9', 'span9_shortcode' );
+
+// [span10]
+function span10_shortcode( $atts ) {
+    return '<div class="span10">';
+}
+add_shortcode( 'span10', 'span10_shortcode' );
+
+// [span11]
+function span11_shortcode( $atts ) {
+    return '<div class="span11">';
+}
+add_shortcode( 'span11', 'span11_shortcode' );
+
+// [span12]
+function span12_shortcode( $atts ) {
+    return '<div class="span12">';
+}
+add_shortcode( 'span12', 'span12_shortcode' );
+
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== 1180px Shortcodes ===
+Contributors: Chris Blackwell
+Tags: 1180px, css, shortcodes
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+1180px shortcodes gives you simple shortcodes to use the 1180px css grid system.
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Shortcode Usage ==
+
+The 1180 grid system is a 12-column grid, so all column spans must add up to 12.
+
+For example, you can gave [span8] and [span4] in the same row,
+or you can have three [span4] for a 3-column layout.
+
+To keep things well spaced out, and for automatic clearing of the floated columns, wrap
+your span shortcodes in a row shortcode like the example below.
+
+Example 3 column grid
+----------------------
+
+    [row]
+        [span4]
+            Column One
+        [/span4]
+
+        [span4]
+            Column Two
+        [/span4]
+
+        [span4]
+            Column Three
+        [/span4]
+    [/row]
+
+
+## Adding classes to the row
+You can add optional classes to the row shortcode. You can also set the class to flexbox,
+for full flexbox support in browsers. If the browser does not support flexbox, it will
+fallback to floats.
+
+**Examples**
+
+    [row class="flexbox"]
+
+    [row class="flexbox someOtherClass anotherClass"]
+
+
+== Changelog ==
+
+* April 5th, 2013
+  - Changed 1180px css file to 1.0
+
+* April 4th, 2013
+  - Added basic shortcode support
+  - Included most recent version of compressed 1180px css
